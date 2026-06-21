80479e9d54118a746178e95cc68ea269e736a229
Parneet <parneetsingh022@gmail.com>
2026-06-21 15:45:08 +0530

    moved notify test to cli_tests.rs

--- tests/cli_tests.rs
+++ tests/cli_tests.rs
@@ -82,4 +82,21 @@ mod tests {
             .failure()
             .stderr(predicates::str::contains("invalid/file/path"));
     }
+
+    #[test]
+    fn notify_step_produces_right_output() {
+        let mut cmd = Command::cargo_bin("rulix").unwrap();
+
+        let output = cmd
+            .args(["run", "--rules", "tests/fixtures/notify_step.yaml"])
+            .assert()
+            .success()
+            .get_output()
+            .stdout
+            .clone();
+
+        let stdout = String::from_utf8(output).unwrap();
+
+        insta::assert_snapshot!(stdout);
+    }
 }
